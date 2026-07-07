79fe63bba3a38d8b9556a0a828c21daca20abdec
Nasir-Jameel <beuroairer@gmail.com>
2026-07-07 05:03:05 +0500

    error has been fixed

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
     }
   },
   plugins: [
+    react(),
     tailwindcss(),
   ],
   resolve: {
